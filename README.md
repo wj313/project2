a7dc0213908c7f516990f84edd1beedc88586cf8
Mike Duggan <mike@groogrux.dhcp.nd.edu>
2013-10-17 12:47:41 -0400

    created initial data struct

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 project2
 ========
+Mike Duggan and Waleed Johnson
 
+all shared files in 'shared' folder
 project for compute networking
---
+++ shared/sensor.h
@@ -0,0 +1,19 @@
+//header for shared sensor struct
+//Mike Duggan and Waleed Johnson
+//
+
+#ifndef SENSOR_H
+#define SENSOR_H
+
+typedef struct Sensor {
+	int numberSensors;//total number of sensors on host
+	int currentSensor; //sensor number
+	double data;//sensor data from sensor
+	double lowvalue;//low value from sensor config
+	double highvalue;//high value from sensor config
+	char timestamp[32];//time stamp string
+	int actionrequested; //action requested
+
+} Sensor;
+
+#endif
